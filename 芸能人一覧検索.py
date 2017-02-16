9a7a52043389553d720a164e8017631c3ece455d
hkenn <kento.hashizumeee@gmail.com>
2017-02-16 10:43:31 +0900

    初めてのGitHub！

--- 芸能人一覧検索.py
+++ 芸能人一覧検索.py
@@ -1,25 +1,21 @@
 
 # coding: utf-8
 
-# In[10]:
-
 from bs4 import BeautifulSoup
 from urllib.request import urlopen 
 
 #芸能人一覧
 url = "http://www.weblio.jp/ontology/%E6%97%A5%E6%9C%AC%E3%81%AE%E8%8A%B8%E8%83%BD%E4%BA%BA_1"
 
-# urlopen()でデータを取得 --- (※1)
+# urlopen()でデータを取得
 res = urlopen(url)
 
-# BeautifulSoupで解析 --- (※2)
+# BeautifulSoupで解析
 soup = BeautifulSoup(res, "html.parser")
 
-# 日本の芸能人一覧を抽出 --- (※1)
+# 日本の芸能人一覧を抽出
 actor_actoress_Japan = soup.select(".subCatWordsB")
 
-
-# In[12]:
 
 #名前の取り出しと列挙
 for a in actor_actoress_Japan:
@@ -27,7 +23,6 @@ for a in actor_actoress_Japan:
     print(title)
 
 
-# In[ ]:
 
 
 
